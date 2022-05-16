0652b71e2effff32ee6da6fea39f6d26fae72e22
atmaja32 <37374785+atmaja32@users.noreply.github.com>
2022-05-16 14:30:24 -0400

    Update script-profile.js

--- assets/js/script-profile.js
+++ assets/js/script-profile.js
@@ -17,25 +17,23 @@ function getUploadResume() {
   userid = document.getElementById("username").value;
 
   var params = {
-    'username' : userid
+    username: userid,
   };
 
   var add_params = {
     headers: {
-      'Content-Type': 'application/json',
-    }
+      "Content-Type": "application/json",
+    },
   };
 
   console.log(params);
   console.log(add_params);
 
   // json_data = getJobData(params)
-  apiClient.jobGet(params, '', add_params)
-  .then(function(res){
-    console.log("Response: ",res);
-  })
+  apiClient.jobGet(params, "", add_params).then(function (res) {
+    console.log("Response: ", res);
+  });
 }
-
 
 function onFormSubmit() {
   if (validate()) {
@@ -51,38 +49,27 @@ function onFormSubmit() {
   }
 }
 
-
 function uploadResume() {
-  var file = document.getElementById('file').files[0];
-  var file_data;
+  var file = document.getElementById("file").files[0];
   console.log(file);
-  var encoded_image = getBase64(file).then((data) => {
-      console.log(data);
-      var apigClient = apigClientFactory.newClient();
+  var apigClient = apigClientFactory.newClient();
 
-      var file_type = file.type + ';base64';
-      //var file_type = file.type;
-
-      console.log(file.type);
-
-      var body = data;
-      var params = {
-          key: file.name,
-          bucket: 'user-resumes-bucket',
-          'Content-Type': file.type,
-          Accept: 'file/*',
-      };
-      var additionalParams = {};
-      apigClient
-      .uploadfileBucketKeyPut(params, body, additionalParams)
-      .then(function (res) {
-          if (res.status == 200) {
-          document.getElementById('uploadText').innerHTML =
-              'Resume Uploaded  !!!';
-          document.getElementById('uploadText').style.display = 'block';
-          }
+  var params = {
+    key: file.name,
+    bucket: "user-resumes-bucket",
+    "Content-Type": file.type,
+    Accept: "file/*",
+  };
+  var additionalParams = {};
+  apigClient
+    .uploadfileBucketKeyPut(params, body, additionalParams)
+    .then(function (res) {
+      if (res.status == 200) {
+        document.getElementById("uploadText").innerHTML =
+          "Resume Uploaded  !!!";
+        document.getElementById("uploadText").style.display = "block";
+      }
     });
-});
 }
 
 function validate() {
@@ -100,4 +87,46 @@ function validate() {
       document.getElementById("fullNameValidationError").classList.add("hide");
   }
   return isValid;
+}
+
+// ------ FILE UPLOAD ------ //
+//Bucket Configurations
+var bucketName = "user-resumes-bucket";
+var bucketRegion = "us-east-1";
+var IdentityPoolId = "us-east-1:3876d161-789b-4f34-9dc2-03cf80d34457";
+
+AWS.config.update({
+  region: bucketRegion,
+  credentials: new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: IdentityPoolId,
+  }),
+});
+
+var s3 = new AWS.S3({
+  apiVersion: "2006-03-01",
+  params: { Bucket: bucketName },
+});
+
+function s3upload() {
+  var files = document.getElementById("fileUpload").files;
+  if (files) {
+    var file = files[0];
+    var fileName = file.name;
+    var filePath = "my-first-bucket-path/" + fileName;
+    var fileUrl =
+      "https://" + bucketRegion + ".amazonaws.com/my-first-bucket/" + filePath;
+    s3.upload(
+      {
+        Key: filePath,
+        Body: file,
+        ACL: "public-read",
+      },
+      function (err, data) {
+        if (err) {
+          reject("error");
+        }
+        alert("Successfully Uploaded!");
+      }
+    );
+  }
 }
